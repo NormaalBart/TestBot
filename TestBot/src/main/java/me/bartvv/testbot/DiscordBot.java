b333099fc1e514288f4379850bc87be9211945ff
NormaalBart <bartkempen10@gmail.com>
2018-06-07 17:15:29 +0200

    Added playing game

--- TestBot/src/main/java/me/bartvv/testbot/DiscordBot.java
+++ TestBot/src/main/java/me/bartvv/testbot/DiscordBot.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class DiscordBot {
@@ -16,6 +17,7 @@ public class DiscordBot {
 
 	public static void main( String[] args ) {
 		JDABuilder builder = new JDABuilder( AccountType.BOT );
+		builder.setGame( Game.of( "Creating a bot" ) );
 		builder.setToken( Utils.getToken() );
 		builder.setAutoReconnect( true );
 		builder.setStatus( OnlineStatus.ONLINE );
